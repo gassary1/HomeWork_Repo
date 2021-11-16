464007591bb6f5b0101c46600d04e5590c695ce4
gassary1 <94483212+gassary1@users.noreply.github.com>
2021-11-16 23:36:02 +0300

    Add files via upload
    Home Work №6

---
+++ HomeWork_6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HomeWork_3
+{
+    class HomeWork_6
+    {
+        static void Main(string[] args)
+        {
+            string name, surname, specialization;
+            int age;
+
+            Console.Write("Введите ваше имя: ");
+            name = Console.ReadLine();
+            Console.Write("Введите вашу фамилию: ");
+            surname = Console.ReadLine();
+            Console.Write("Введите ваш возраст: ");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите вашей специализацию: ");
+            specialization = Console.ReadLine();
+
+            Console.WriteLine($"Ваше имя - {name}. Ваша фамилия - {surname}." +
+                $"\nВаш возраст составляет {age} лет.\nСпециализация - {specialization}");
+        }
+    }
+}
