8f51d6457b71512c99ed5ef40fd90939bc91aa77
gassary1 <94483212+gassary1@users.noreply.github.com>
2021-11-16 23:05:16 +0300

    Add files via upload
    Home Work №3

---
+++ HomeWork_3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HomeWork_3
+{
+    class HomeWork_2
+    {
+        static void Main(string[] args)
+        {
+            int countOfPhoto = 52;
+            int cols = 3;
+
+            int fullRow = countOfPhoto / cols;
+            int remainOfPhoto = countOfPhoto % cols;
+
+            Console.WriteLine($"Всего заполненных рядов - {fullRow}\nОстаток фотографии - {remainOfPhoto}");
+            Console.ReadLine();
+        }
+    }
+}
