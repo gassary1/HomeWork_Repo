747cd96cf7b26f74b89db6ca1aee4d0a37fd4acd
gassary1 <94483212+gassary1@users.noreply.github.com>
2021-11-16 23:16:05 +0300

    Add files via upload
    Home Work №4

---
+++ HomeWork_4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HomeWork_3
+{
+    class HomeWork_4
+    {
+        static void Main(string[] args)
+        {
+            int gold;
+            int counOfDiamond;
+            int price = 15;
+
+            Console.Write("Введите имеющееся количество золота: ");
+            gold = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"Добро пожаловать! Сегодня алмазы стоят {price} золотых.\nСколько алмахов вы хотите купить? ");
+            counOfDiamond = Convert.ToInt32(Console.ReadLine());
+
+            gold -= price * counOfDiamond;
+
+            Console.WriteLine($"Вы купили {counOfDiamond} алмазов. У вас осталось {gold} золота");
+            Console.ReadLine();
+
+        }
+    }
+}
