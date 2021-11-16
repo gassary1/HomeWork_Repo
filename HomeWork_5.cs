577e3d37065ceeed8cd7306d0c0126376b6a960f
gassary1 <94483212+gassary1@users.noreply.github.com>
2021-11-16 23:25:31 +0300

    Add files via upload

---
+++ HomeWork_5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HomeWork_3
+{
+    class HomeWork_5
+    {
+        static void Main(string[] args)
+        {
+            int countOfPepole;
+            int timeOfService = 10;
+            int timeOfWaiting;
+
+            Console.Write("Введите количество людей: ");
+            countOfPepole = Convert.ToInt32(Console.ReadLine());
+
+            timeOfWaiting = countOfPepole * timeOfService;
+
+            Console.WriteLine($"Ожидание в очереди составит {timeOfWaiting / 60} ч. и {timeOfWaiting % 60} м.");
+        }
+    }
+}
