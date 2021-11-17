1d735eb8d576d3853ea4dd099cc4b5bf5caf611b
gassary1 <94483212+gassary1@users.noreply.github.com>
2021-11-17 20:44:43 +0300

    Add files via upload

---
+++ HomeWork_8.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HomeWork_8
+{
+    class HomeWork_8
+    {
+        static void Main(string[] args)
+        {
+            int countOfRepeat = 10;
+
+            Console.WriteLine("Для выхода из программы наберите exit");
+
+            while (true)
+            {
+                Console.Write("Введите любое сообщение: ");
+                string message = Console.ReadLine();
+                Console.WriteLine(message);
+
+                if (message == "exit") break;
+            }
+
+        }
+    }
+}
