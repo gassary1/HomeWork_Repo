7f1adc505d5f223f86ec468fa03d249643adda71
gassary1 <94483212+gassary1@users.noreply.github.com>
2021-11-17 20:32:07 +0300

    Add files via upload

---
+++ HomeWork_7.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HomeWork_7
+{
+    class HomeWork_7
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World";
+            int countOfRepeat = 10;
+
+            for (int i=0; i<countOfRepeat; i++)
+            {
+                Console.WriteLine(message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
