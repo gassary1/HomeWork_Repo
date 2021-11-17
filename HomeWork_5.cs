54dae5d089f75abb14587ccf2ed55b99b7a73667
gassary1 <94483212+gassary1@users.noreply.github.com>
2021-11-17 20:16:47 +0300

    Update HomeWork_5.cs

--- HomeWork_5.cs
+++ HomeWork_5.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace HomeWork_3
 {
@@ -9,13 +9,17 @@ namespace HomeWork_3
             int countOfPepole;
             int timeOfService = 10;
             int timeOfWaiting;
+            int hoursOfWaiting;
+            int minutesOfWaiting;
 
             Console.Write("Введите количество людей: ");
             countOfPepole = Convert.ToInt32(Console.ReadLine());
 
             timeOfWaiting = countOfPepole * timeOfService;
+            hoursOfWaiting = timeOfWaiting / 60;
+            minutesOfWaiting = timeOfWaiting % 60;
 
-            Console.WriteLine($"Ожидание в очереди составит {timeOfWaiting / 60} ч. и {timeOfWaiting % 60} м.");
+            Console.WriteLine($"Ожидание в очереди составит {hoursOfWaiting} ч. и {minutesOfWaiting} м.");
         }
     }
 }
