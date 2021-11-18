47f5c9c9254513ebed915d5daeffb841dbd8a6a4
gassary1 <94483212+gassary1@users.noreply.github.com>
2021-11-19 00:22:18 +0300

    Add files via upload

---
+++ HomeWork_12.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HomeWork_12
+{
+    class HomeWork_12
+    {
+        static void Main(string[] args)
+        {
+            string password = "020395";
+            string userInput = string.Empty;
+            int tryCount = 3;
+
+            for (int i = 0; i < tryCount; i++)
+            {
+                Console.Write($"Для досутпа у вас есть {tryCount} попыток. Введите пароль: ");
+                userInput = Console.ReadLine();
+
+                if (userInput == password)
+                {
+                    Console.WriteLine("Торжественно клянусь, что замышляю только шалость");
+                }
+                else
+                {
+                    Console.WriteLine($"Доступ запрещен. Количество попыток равно - {tryCount-i-1}");
+                }
+            }
+        }
+    }
+}
