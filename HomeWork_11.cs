462f5f3212f36f044cf5b71796695ef605692452
gassary1 <94483212+gassary1@users.noreply.github.com>
2021-11-18 23:20:38 +0300

    Add files via upload

---
+++ HomeWork_11.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HomeWork_11
+{
+    class HomeWork_11
+    {
+        static void Main(string[] args)
+        {
+            int result;
+
+            for (int i = 1; i < 15; i++)
+            {
+                result = 7 * i;
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
