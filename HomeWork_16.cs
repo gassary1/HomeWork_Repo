e4b9d586b316a791c8ad10288224bce841e130c2
gassary1 <94483212+gassary1@users.noreply.github.com>
2021-11-23 00:04:13 +0300

    Add files via upload

---
+++ HomeWork_16.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HomeWork_16
+{
+    class HomeWork_16
+    {
+        static void Main(string[] args)
+        {
+            int[] matrix = new int[30];
+            Random random = new Random();
+
+            Console.WriteLine("Дан массив чисел:\n");
+
+            for (int i=0; i<matrix.Length; i++)
+            {
+                matrix[i] = random.Next(1, 10);
+                Console.Write($"{matrix[i], 3}");
+            }
+
+            Console.WriteLine("\n\nЛокальные максимумы данного массива:\n");
+
+            for (int i = 1; i < matrix.Length-1; i++)
+            {
+                if (matrix[i] > matrix[i - 1] && matrix[i] > matrix[i + 1])
+                {
+                    Console.Write($"{matrix[i],5}");
+                }
+            }
+        }
+    }
+}
