220993340a97e2936be8e88447ff9091662d14fc
gassary1 <94483212+gassary1@users.noreply.github.com>
2021-11-22 17:54:13 +0300

    Add files via upload

---
+++ HomeWork_14.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HomeWork_14
+{
+    class HomeWork_14
+    {
+        static void Main(string[] args)
+        {
+            int column = 1;
+            int row = 2;
+            int sum = 0;
+            int multiplication = 1;
+
+            Random random = new Random();
+
+            int[,] matrix = new int[3, 3];
+
+            Console.WriteLine("Мсходная матрица:");
+            for (int i = 0; i < matrix.GetLength(0); i++) 
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(3, 26);
+                    Console.Write($"{matrix[i, j],5}");
+                }
+                Console.WriteLine();
+            }
+
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                sum += matrix[row-1, j];
+            }
+
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                multiplication *= matrix[i, column-1];
+            }
+
+            Console.WriteLine($"Результат сложения строки №{row} - {sum}");
+            Console.WriteLine($"Результат умножения столбца №{column} - {multiplication}");
+        }
+    }
+}
