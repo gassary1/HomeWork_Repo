af10b04d5233ac09a318bb9ed13e7f670a5aae35
gassary1 <94483212+gassary1@users.noreply.github.com>
2021-11-22 23:40:48 +0300

    Add files via upload

---
+++ HomeWork_15.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace HomeWork_15
+{
+    class HomeWork_15
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[10, 10];
+            int maxNumber = 0;
+            Random random = new Random();
+
+            Console.WriteLine("Исходная матрица:\n");
+
+            for (int i=0; i<matrix.GetLength(0); i++)
+            {
+                for (int j=0; j<matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(6, 27);
+                    if (maxNumber < matrix[i, j])
+                    {
+                        maxNumber = matrix[i, j];
+                    }
+                    Console.Write($"{matrix[i, j], 5}");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"\nМаксимальное число в массиве равно - {maxNumber}. Полученная матрица:\n");
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i,j]==maxNumber)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                    Console.ResetColor();
+                    Console.Write($"{matrix[i, j], 5}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
