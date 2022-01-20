08769934c05e464c6eb101ac0108e2ff14eea338
gassary1 <94483212+gassary1@users.noreply.github.com>
2022-01-20 21:14:31 +0300

    Add files via upload

---
+++ HomeWork_20.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HomeWork_20
+{
+    class HomeWork_20
+    {
+        static void Main(string[] args)
+        {
+            DrawBar(75, 10, ConsoleColor.Red, 0, '_');
+            DrawBar(40, 10, ConsoleColor.Blue, 1, '_');
+
+            static void DrawBar(uint value, uint maxValue, ConsoleColor color, int position, char symbol)
+            {
+                Console.SetCursorPosition(0, position);
+                Console.Write("[");
+
+                ConsoleColor defaultColor = Console.BackgroundColor;
+                Console.BackgroundColor = color;
+
+                for (int i = 0; i < (value * maxValue) / 100; i++)
+                {
+                    Console.Write(symbol);
+                }
+
+                Console.BackgroundColor = defaultColor;
+
+                for (int i = 0; i < maxValue - (value * maxValue) / 100; i++)
+                {
+                    Console.Write(symbol);
+                }
+
+                Console.Write("]");
+            }
+        }
+    }
+}
