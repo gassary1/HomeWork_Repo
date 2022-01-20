822ce3fc848875de7a999d0294a52e3d26923f3e
gassary1 <94483212+gassary1@users.noreply.github.com>
2022-01-20 23:50:36 +0300

    Add files via upload

---
+++ HomeWork_21.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HomeWork_21
+{
+    class HomeWork_21
+    {
+        static int TryConvertToInt32() 
+        {
+            while (true)
+            {
+                string userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out int result))
+                {
+                    return result;
+                }
+
+                else 
+                {
+                    Console.WriteLine("Ошибка ввода");
+                } 
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int a = TryConvertToInt32();
+        }
+    }
+}
