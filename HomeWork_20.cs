61fde3068399f2cc661c5fcb772f6d2792e5824c
gassary1 <94483212+gassary1@users.noreply.github.com>
2022-01-26 00:35:42 +0300

    Update HomeWork_20.cs

--- HomeWork_20.cs
+++ HomeWork_20.cs
@@ -6,25 +6,27 @@ namespace HomeWork_20
     {
         static void Main(string[] args)
         {
-            DrawBar(75, 10, ConsoleColor.Red, 0, '_');
+            DrawBar(100, 10, ConsoleColor.Red, 0, '_');
             DrawBar(40, 10, ConsoleColor.Blue, 1, '_');
 
-            static void DrawBar(uint value, uint maxValue, ConsoleColor color, int position, char symbol)
+            static void DrawBar(uint percent, uint maxValue, ConsoleColor color, int position, char symbol)
             {
+                double value = ((double)percent / 100) * maxValue;
+
                 Console.SetCursorPosition(0, position);
                 Console.Write("[");
 
                 ConsoleColor defaultColor = Console.BackgroundColor;
                 Console.BackgroundColor = color;
 
-                for (int i = 0; i < (value * maxValue) / 100; i++)
+                for (int i = 0; i < value && value > 0 && value < 100; i++)
                 {
                     Console.Write(symbol);
                 }
 
                 Console.BackgroundColor = defaultColor;
 
-                for (int i = 0; i < maxValue - (value * maxValue) / 100; i++)
+                for (double i = value; i < maxValue; i++)
                 {
                     Console.Write(symbol);
                 }
