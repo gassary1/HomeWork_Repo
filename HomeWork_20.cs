688727bd2b8cb96dd4e3f0af18657199e84e442a
gassary1 <94483212+gassary1@users.noreply.github.com>
2022-01-26 20:43:20 +0300

    Update HomeWork_20.cs

--- HomeWork_20.cs
+++ HomeWork_20.cs
@@ -8,32 +8,32 @@ namespace HomeWork_20
         {
             DrawBar(0, 10, ConsoleColor.Red, 0, '_');
             DrawBar(41, 10, ConsoleColor.Blue, 1, '_');
+        }
 
-            static void DrawBar(uint percent, uint maxValue, ConsoleColor color, int position, char symbol)
+        static void DrawBar(uint percent, uint maxValue, ConsoleColor color, int position, char symbol)
+        {
+            int constant = 100;
+            double value = ((double)percent / constant) * maxValue;
+
+            Console.SetCursorPosition(0, position);
+            Console.Write("[");
+
+            ConsoleColor defaultColor = Console.BackgroundColor;
+            Console.BackgroundColor = color;
+
+            for (int i = 0; i < Math.Round(value) && value > 0 && value <= maxValue; i++)
             {
-                int constant = 100;
-                double value = ((double)percent / constant) * maxValue;
-
-                Console.SetCursorPosition(0, position);
-                Console.Write("[");
-
-                ConsoleColor defaultColor = Console.BackgroundColor;
-                Console.BackgroundColor = color;
-
-                for (int i = 0; i < Math.Round(value) && value > 0 && value <= maxValue; i++)
-                {
-                    Console.Write(symbol);
-                }
-
-                Console.BackgroundColor = defaultColor;
-
-                for (double i = Math.Round(value); i < maxValue; i++)
-                {
-                    Console.Write(symbol);
-                }
-
-                Console.Write("]");
+                Console.Write(symbol);
             }
+
+            Console.BackgroundColor = defaultColor;
+
+            for (double i = Math.Round(value); i < maxValue; i++)
+            {
+                Console.Write(symbol);
+            }
+
+            Console.Write("]");
         }
     }
 }
