bc0570cae9ef4a354fa641cf70922a97fd74ba0e
gassary1 <94483212+gassary1@users.noreply.github.com>
2022-01-27 13:12:00 +0300

    Update HomeWork_23.cs

--- HomeWork_23.cs
+++ HomeWork_23.cs
@@ -10,34 +10,44 @@ namespace HomeWork_23
             int[] numbers = new int[random.Next(4, 6)];
 
             Console.WriteLine("Исходный массив:");
-            DrawArray(numbers);
+            FillArray(numbers);
+            PrintArray(numbers);
 
             Console.WriteLine("\nМассив после перемешивания:");
             Shuffle(numbers);
+            PrintArray(numbers);
         }
 
         static void Shuffle(int[] array)
         {
+            int randomIndex;
+            int temp;
             Random random = new Random();
 
             for (int i = array.Length - 1; i >= 0; i--)
             {
-                int j = random.Next(i+1);
-                int temp = array[j];
-                array[j] = array[i];
+                randomIndex = random.Next(i + 1);
+                temp = array[randomIndex];
+                array[randomIndex] = array[i];
                 array[i] = temp;
-                Console.Write($"{array[i], 2}");
             }
         }
-
-        static void DrawArray(int[] array)
+        
+        static void FillArray(int[] array)
         {
             Random random = new Random();
 
             for (int i = 0; i < array.Length; i++)
             {
                 array[i] = random.Next(1, 10);
-                Console.Write($"{array[i], 2}");
+            }
+        }
+
+        static void PrintArray(int[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.Write($"{item, 2}");
             }
         }
     }
