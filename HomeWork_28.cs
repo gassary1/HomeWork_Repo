01f292ce6805576bbf2c1b55f82d4155379c1043
gassary1 <94483212+gassary1@users.noreply.github.com>
2022-02-21 21:11:11 +0300

    Update HomeWork_28.cs

--- HomeWork_28.cs
+++ HomeWork_28.cs
@@ -9,17 +9,17 @@ namespace HomeWork_28
         {
             List<Player> players = new List<Player>();
 
-            Player player1 = new Player("Riko");
-            Player player2 = new Player("Artas", "Warrior", 10);
-            Player player3 = new Player("Simon", 16);
-            Player player4 = new Player("Nikolas", "Druid");
+            Player player1 = new Player("Riko","Dwarf", 1);
+            Player player2 = new Player("Artas", "Human", 10);
+            Player player3 = new Player("Simon", "Nord", 16);
+            Player player4 = new Player();
 
             players.Add(player1);
             players.Add(player2);
             players.Add(player3);
             players.Add(player4);
 
-            Console.WriteLine($"{"Имя",15} {"Класс",15} {"Уровень",5}");
+            Console.WriteLine($"{"Имя",15} {"Расса",15} {"Уровень",5}");
 
             foreach (var user in players)
             {
@@ -30,53 +30,53 @@ namespace HomeWork_28
 
     class Player
     {
-        #region Constructors
-        public Player(string Name, string Class, uint Level)
-        {
-            _name = Name;
-            _class = Class;
-            _level = Level;
-        }
-
-        public Player(string Name, string Class)
-        {
-            _name = Name;
-            _class = Class;
-        }
-
-        public Player(string Name, uint Level)
-        {
-            _name = Name;
-            _level = Level;
-        }
-
-        public Player(string Name)
-        {
-            _name = Name;
-        }
-        #endregion
-
-        #region Metods
-        public void PrintInfo()
-        {
-            Console.WriteLine($"{Name,15} {Class,15} {Level,5}");
-        }
-        #endregion
-
-        #region Properties
-        public string Name { get { return _name; } }
-
-        public uint Level { get { return _level; } set { value = _level; } }
-
-        public string Class { get { return _class; } }
-        #endregion
-
         #region Fields
         private string _name;
 
-        private uint _level;
+        private string _race;
 
-        private string _class;
+        private uint _level;
+        #endregion
+
+        #region Properties
+        public string Name => _name;
+
+        public string Race => _race;
+
+        public uint Level => _level;
+        #endregion
+
+        #region Constructors
+        public Player(string name, string race, uint level)
+        {
+            _name = name;
+            _race = race;
+            _level = level;
+        }
+
+        public Player() : this("", "", 0) { }
+        #endregion
+
+        #region Metods
+        public string ChangeRace(string newRace)
+        {
+            return _race = newRace;
+        }
+
+        public string ChangeName(string newName)
+        {
+            return _name = newName;
+        }
+
+        public void AddLevel()
+        {
+            _level++;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{Name,15} {Race,15} {Level,5}");
+        }
         #endregion
     }
 }
