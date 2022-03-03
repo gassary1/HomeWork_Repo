9b9acb9606f6e41e38498e9f856f22abee07dbe5
gassary1 <94483212+gassary1@users.noreply.github.com>
2022-03-03 22:25:31 +0300

    Update HomeWork_30.cs

--- HomeWork_30.cs
+++ HomeWork_30.cs
@@ -7,17 +7,91 @@ namespace HomeWork_30
     {
         static void Main(string[] args)
         {
+            string name;
+            string nickname;
+            int playerPosition;
+            int countNotes;
             bool isActive = true;
-            Repository repository = new Repository(10);
-            repository.PrintRepository();
-            Console.WriteLine();
-            repository.PrintRepository();
+            ConsoleKey useroption;
 
+            Console.WriteLine("Введите количество записей (если значение будет больше 0, то произойдет автоматическое заполнение ячеек): ");
+            countNotes = GetNumber();
+            Repository repository = new Repository(countNotes);
+
+            Console.Clear();
+
+            while (isActive)
+            {
+                repository.PrintRepository();
+                PrintMenu();
+                useroption = Console.ReadKey().Key;
+                Console.Clear();
+
+                switch (useroption)
+                {
+                    case ConsoleKey.D1:
+                        Console.Write("Введите имя: ");
+                        name = Console.ReadLine();
+                        Console.Write("Введите ник игрока: ");
+                        nickname = Console.ReadLine();
+                        repository.AddPlayer(name, nickname);
+                        break;
+
+                    case ConsoleKey.D2:
+                        Console.Write("Введите номер позиции игрока: ");
+                        playerPosition = GetPosition();
+                        repository[playerPosition].Ban();
+                        break;
+
+                    case ConsoleKey.D3:
+                        Console.Write("Введите номер позиции игрока: ");
+                        playerPosition = GetPosition();
+                        repository.DeletePlayer(playerPosition);
+                        break;
+
+                    case ConsoleKey.D4:
+                        isActive = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Ошибка ввода");
+                        break;
+                }
+            }
         }
 
         static void PrintMenu()
         {
+            Console.WriteLine();
             Console.WriteLine("1 - Добавить игрока\n2 - Забанить игрока по номеру\n3 - удалить игрока\n4 - Выход");
+        }
+
+        static int GetNumber()
+        {
+            bool isActive = true;
+            int result = 0;
+
+            while (isActive)
+            {
+                string userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput,out result))
+                {
+                    isActive = false;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка ввода");
+                }
+            }
+            return result;
+        }
+
+        static int GetPosition()
+        {
+            int playerPosition = GetNumber();
+            int currentPosition = playerPosition - 1;
+            return currentPosition;
         }
     }
 
@@ -26,7 +100,6 @@ namespace HomeWork_30
         private string _name;
         private string _nickname;
         private bool _flag;
-        //static uint indexName;
         static Random random;
         static List<string> dbNicknames;
 
@@ -36,7 +109,6 @@ namespace HomeWork_30
 
         static Player()
         {
-            //indexName = 1;
             random = new Random();
             dbNicknames = new List<string>();
         }
@@ -47,12 +119,12 @@ namespace HomeWork_30
             _nickname = nickname;
             _flag = flag;
 
-            if(nickname == string.Empty || Player.dbNicknames.Contains(nickname))
+            if (_nickname == string.Empty || Player.dbNicknames.Contains(_nickname))
             {
-                nickname = $"{Guid.NewGuid().ToString().Substring(0, 5)}";
+                _nickname = $"{Guid.NewGuid().ToString().Substring(0, 5)}";
             }
 
-            Player.dbNicknames.Add(nickname);
+            Player.dbNicknames.Add(_nickname);
         }
 
         public Player() : this("", "", false) { }
@@ -88,17 +160,7 @@ namespace HomeWork_30
         private List<Player> _players;
         static Random random;
         static readonly string[] names;
-        static readonly bool flags;
-
-        public Repository(uint count)
-        {
-            CreatePlayers();
-
-            for (int i = 0; i < count; i++)
-            {
-                _players.Add(new Player());
-            }
-        }
+        private readonly bool randomFlag;
 
         static Repository()
         {
@@ -118,15 +180,54 @@ namespace HomeWork_30
             };
         }
 
+        public Repository(int count)
+        {
+            CreatePlayers();
+
+            for (int i = 0; i < count; i++)
+            {
+                _players.Add(new Player(names[random.Next(names.Length)], "", randomFlag));
+                randomFlag = random.Next(2) == 1 ? true : false;
+            }
+        }
+
+        public Player this[int index]
+        {
+            get { return _players[index]; }
+            set { _players[index] = value; }
+        }
+
         public void CreatePlayers()
         {
             _players = new List<Player>();
         }
 
+        public void AddPlayer(string name, string nickname)
+        {
+            _players.Add(new Player(name, nickname, false));
+        }
+
+        public void DeletePlayer(int position)
+        {
+            if (position > 0)
+            {
+                _players.Remove(_players[position]);
+            }
+            else
+            {
+                Console.WriteLine("Неверный индекс игрока");
+            }
+        }
+
         public void PrintRepository()
         {
+            int playerPosition = 1;
+
+            Console.WriteLine($"{"Имя",10} {"Ник",10} {"Статус бана",12}");
+
             foreach (var player in _players)
             {
+                Console.Write(playerPosition++);
                 player.PrintInfo();
             }
         }
