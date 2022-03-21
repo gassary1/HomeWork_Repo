99f2b1f2fa8e4173ebd07d1a62475aea84b5b7d6
gassary1 <94483212+gassary1@users.noreply.github.com>
2022-03-21 23:42:55 +0300

    Update HomeWork_31.cs

--- HomeWork_31.cs
+++ HomeWork_31.cs
@@ -1,50 +1,40 @@
 using System;
-using System.Collections.Generic;
 
-namespace HomeWork_31
+namespace ConsoleApp2
 {
     class Program
     {
-        static void Main(string[] Args)
+        static void Main(string[] args)
         {
+            Player firstPlayer = new Player(3, 10);
+            firstPlayer.DrawPlayer('@');
 
+            Player secondPlayer = new Player(6, 5);
+            secondPlayer.DrawPlayer('*');
+
+            Console.ReadLine();
         }
     }
 
     class Player
     {
-        private string _name;
-        private int _gold;
+        private int _xPosition;
+        private int _yPosition;
 
-        public string Name => _name;
-        public int Gold => _gold;
-    }
+        public int XPosition => _xPosition;
+        public int YPosition => _yPosition;
 
-    class Seller
-    {
-        private string _name;
-        private int _gold;
-        private List<Product> _products;
-
-        public string Name => _name;
-        public int Gold => _gold;
-    }
-
-    class Product
-    {
-        private string _name;
-        private int _amount;
-        private int _price;
-
-        public string Name => _name;
-        public int Amount => _amount;
-        public int Price => _price;
-
-        public Product(string name, int amount, int price)
+        public Player(int xPosition, int yPosition)
         {
-            _name = name;
-            _amount = amount;
-            _price = price;
+            _xPosition = xPosition;
+            _yPosition = yPosition;
+        }
+
+        public void DrawPlayer(char playerSimbol)
+        {
+            Console.SetCursorPosition(XPosition, YPosition);
+
+            Console.WriteLine(playerSimbol);
         }
     }
 }
