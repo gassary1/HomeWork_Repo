612fd440e3a57e76c9316f4f8857460186d1de86
gassary1 <94483212+gassary1@users.noreply.github.com>
2022-03-22 22:39:16 +0300

    Create HomeWork_new3.cs

---
+++ HomeWork_new3.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number = random.Next(1,28);
+            int endNumber = 1000;
+            int multiply;
+
+            Console.WriteLine($"Сгенерированное число: {number}");
+            Console.WriteLine($"Числа, кратные {number}:");
+
+            for (int i = 100; i < endNumber; i++)
+            {
+                for (multiply = 1; number * multiply < 1000; multiply++)
+                {
+                    if (i==number*multiply)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
